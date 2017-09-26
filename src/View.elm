95f13ad89970e829ac12bd02f0e6473436068abe
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2017-09-26 17:50:27 +0200

    Added index.js; moved index.html around; Added first view

---
+++ src/View.elm
@@ -0,0 +1,27 @@
+module View exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Material
+import Material.Scheme
+import Material.Table as Table
+import Material.Button as Button
+import Material.Options as Options exposing (css)
+import Material.Typography as Typo
+import Material.Elevation as Elevation
+
+
+view : Model -> Html Msg
+view model =
+    h1 []
+        [ h1 [] [ text "Generation III" ]
+        , div [ id "thumbnails" ]
+            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
+            , img [ src "http://elm-in-action.com/2.jpeg" ] []
+            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+            ]
+        ]
+
+
+main =
+    view "no model yet"
