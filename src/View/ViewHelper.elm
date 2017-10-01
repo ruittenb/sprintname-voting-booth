93c0086578fe5cd930d553f2a8449ecd1f84df18
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2017-10-01 23:36:17 +0200

    Bring Models and json in agreement; remove unused code

--- src/View/ViewHelper.elm
+++ src/View/ViewHelper.elm
@@ -1,21 +1,6 @@
 module ViewHelper exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
-
-
-{-
-   import Material
-   import Material.Scheme
-   import Material.Table as Table
-   import Material.Button as Button
-   import Material.Options as Options exposing (css)
-   import Material.Typography as Typo
-   import Material.Elevation as Elevation
--}
-
 import Models exposing (..)
-import Msgs exposing (Msg)
 
 
 pokeImageName : String -> String
