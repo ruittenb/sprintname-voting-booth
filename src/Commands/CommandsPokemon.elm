b7f4b308fb02687ad6af3912f9811a434e98daa5
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2017-10-02 00:09:19 +0200

    First work on Commands

--- src/Commands/CommandsPokemon.elm
+++ src/Commands/CommandsPokemon.elm
@@ -4,6 +4,11 @@ import Models exposing (..)
 import Msgs exposing (..)
 
 
+pokemonApiUrl : String
+pokemonApiUrl =
+    "http://pokeapi.co/api/v2/pokemon/"
+
+
 loadPokemon : number -> Pokemon
 loadPokemon num =
     -- Msgs.OnLoadPokemon
--- src/Commands/CommandsVote.elm
+++ src/Commands/CommandsVote.elm
@@ -1,10 +1,13 @@
 module CommandsVote exposing (..)
 
+import RemoteData
+import Json.Encode as Encode
+import Json.Decode as Decode
+import Json.Decode.Pipeline exposing (decode, required)
+import Msgs exposing (Msg)
 import Models exposing (..)
-import Msgs exposing (..)
 
 
-loadPokemon : number -> Pokemon
-loadPokemon num =
-    -- Msgs.OnLoadPokemon
-    missingNo
+loadRatings : List UserRatings
+loadRatings =
+    []
