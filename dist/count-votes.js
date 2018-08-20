3a3119a01646a79f6104a66e8afaf32583d764ee
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2018-08-20 09:41:12 +0200

    Fixed bugs in count-votes

--- dist/count-votes.js
+++ dist/count-votes.js
@@ -1,26 +1,31 @@
 function countVotes() {
     var votes = [];
-    var person;
+    var person, vote;
 
-    jQuery('.rating-nodes > [title]').each(
+    // currently logged in user
+    var currentPersonVotes = jQuery('.rating-nodes > .voting-node > .selected');
+    if (currentPersonVotes.length) {
+        var [ currentPersonName ] = currentPersonVotes[0].title.split(': ');
+        votes[currentPersonName] = currentPersonVotes.length;
+    }
+
+    // other users
+    jQuery('.rating-nodes > [title]:not([title=""])').each(
         function (i, n) {
             [ person, vote ] = n.title.split(': ');
             votes[person] = (votes[person] || 0) + Number(vote);
         }
     );
 
-    var skip = [ 'Brian', 'Ivan', 'Jason' ];
-    var currentPersonVotes = jQuery('.rating-nodes > .voting-node > .selected');
-    var [ currentPersonName ] = currentPersonVotes[0].title.split(': ');
-    votes[currentPersonName] = currentPersonVotes.length;
-
+    // format results
     console.warn('Number of votes on this page:');
+    var skip = [ 'Brian', 'Ivan', 'Jason' ];
     for (var name in votes) {
         if (skip.includes(name)) continue;
-        var logFunc = votes[name] == 6 ? 'info' :
-            votes[name] == 0 ? 'error' : 'warn';
-        console[logFunc](name, ': ', votes[name]);
+        var color = votes[name] == 6 ? 'blue' :
+            votes[name] == 0 ? 'red' : '#bb0';
+        console.log('%c' + name + ': ' + votes[name], 'color:' + color);
     };
-    return void(0);
+    return;
 }
 
