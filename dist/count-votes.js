d040cdce6a059ee28c085fcb7f9684459b0206d4
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2018-08-21 11:30:32 +0200

    Updated countVotes to account for the number of Pokemon that can be voted for

--- dist/count-votes.js
+++ dist/count-votes.js
@@ -17,12 +17,18 @@ function countVotes() {
         }
     );
 
+    var tiles = jQuery('.poketile').length;
+    var expected = (
+        tiles < 1 ? 0 :
+        tiles === 1 ? 3 :
+        tiles === 2 ? 5 : 6);
+
     // format results
     console.warn('Number of votes on this page:');
     var skip = [ 'Brian', 'Ivan', 'Jason' ];
     for (var name in votes) {
         if (skip.includes(name)) continue;
-        var color = votes[name] == 6 ? 'blue' :
+        var color = votes[name] == expected ? 'blue' :
             votes[name] == 0 ? 'red' : '#bb0';
         console.log('%c' + name + ': ' + votes[name], 'color:' + color);
     };
