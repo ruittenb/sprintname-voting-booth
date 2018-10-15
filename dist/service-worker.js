53c3494c4a8bad7de7665a61f23b56078f3ee30a
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2018-10-15 09:44:19 +0200

    Added fleshed out service worker

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,2 +1,55 @@
 
+var version = 'v8.1b'
+var cacheName = 'sprintname-voting-booth-' + version;
+var filesToCache = [
+    '/',
+    '/index.html',
+//    '/app.js',
+    '/voting-booth.css',
+    '/lightbox.css',
+    '/lightbox.min.js',
+    '/images/lb-close.png',
+    '/images/lb-loading.gif',
+    '/images/lb-next.png',
+    '/images/lb-prev.png',
+    '/images/loading-busy.gif',
+    '/images/loading-circle.svg',
+    '/images/loading-error.png',
+    '/images/loading-shade.png',
+    '/icons/cross.png',
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Installing');
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activating');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('[ServiceWorker] Fetching', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
 
