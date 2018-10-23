8fe2b7b74ae3a3a2f743f5bf808c98a9833a75d3
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2018-10-23 10:29:20 +0200

    Added comment

--- src/Constants/Database.elm
+++ src/Constants/Database.elm
@@ -3,6 +3,13 @@ module Constants.Database exposing (..)
 import Models.Database exposing (FirebaseConfig)
 
 
+{- The apiKey essentially just identifies your Firebase project on the Google servers.
+   It is not a security risk for someone to know it. In fact, it is necessary for them
+   to know it, in order for them to interact with your Firebase project.
+   @see https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public
+-}
+
+
 firebaseApiKey : String
 firebaseApiKey =
     "AIzaSyAm4--Q2MjVWGZYW-IC8LPZARXJq-XyHXA"
