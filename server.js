92ba5eccd9b0ad558022d341488ebc1d2c651a22
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2018-11-05 12:20:10 +0100

    Better server output for easy navigation to URL

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const FirebaseTokenServer = require('./tokenserver/tokenserver.js');
 
 const PORT        = 4201;
 const ADDR        = '0.0.0.0';
+const HOST        = (ADDR === '0.0.0.0' ? 'localhost' : ADDR);
+const PROTO       = 'http:';
 const express     = require('express');
 const webserver   = express();
 const tokenserver = new FirebaseTokenServer(webserver);
@@ -14,7 +16,9 @@ const tokenserver = new FirebaseTokenServer(webserver);
 webserver.use(express.static('dist'))
 
 webserver.listen(PORT, ADDR, function () {
-    console.log(`Server is listening on ${ADDR}:${PORT}`);
+    console.log(
+        `Server is listening on ${PROTO}//${HOST}:${PORT}`,
+        (ADDR === '0.0.0.0' ? '(all interfaces)' : ''));
 }).on('error', function (err) {
     console.log(`Unable to listen on ${ADDR}:${PORT} : ${err.code}`);
 });
