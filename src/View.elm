6bfffa7f832db782e47eec37801558811ceedaf3
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2019-08-31 00:56:01 +0200

    View: display buttons also when loading

--- src/View.elm
+++ src/View.elm
@@ -8,23 +8,41 @@ import Models exposing (ApplicationState)
 import Msgs exposing (Msg)
 
 
+alwaysPanes : ApplicationState -> List (Html Msg)
+alwaysPanes state =
+    [ title
+    , applicationPane state
+    ]
+
+
+onlinePanes : ApplicationState -> List (Html Msg)
+onlinePanes state =
+    [ functionPane state
+    , pokemonCanvas state
+    ]
+
+
+getPanes : ApplicationState -> List (Html Msg)
+getPanes state =
+    case state.settings of
+        RemoteData.Success settings ->
+            if (settings.maintenanceMode) then
+                -- Success, maintenance mode
+                []
+            else
+                -- Success, no maintenance mode
+                onlinePanes state
+
+        RemoteData.Failure error ->
+            -- Failure: assume the worst
+            []
+
+        _ ->
+            -- NotAsked, Loading: assume no maintenance
+            onlinePanes state
+
+
 view : ApplicationState -> Html Msg
 view state =
-    let
-        sections =
-            [ title
-            , applicationPane state
-            ]
-                ++ case state.settings of
-                    RemoteData.Success settings ->
-                        if (not settings.maintenanceMode) then
-                            [ functionPane state
-                            , pokemonCanvas state
-                            ]
-                        else
-                            []
-
-                    _ ->
-                        []
-    in
-        div [] sections
+    div []
+        (alwaysPanes state ++ getPanes state)
