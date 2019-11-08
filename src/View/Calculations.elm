a3ccd09b9688987ad263f5a18afaaba0841ec414
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2019-11-08 14:45:37 +0100

    Correctly calculate the number of expected votes as a function of the maximum number of stars

--- src/View/Calculations.elm
+++ src/View/Calculations.elm
@@ -1,6 +1,7 @@
 module View.Calculations exposing (calculatePeopleVotes, calculatePokemonVotes)
 
 import RemoteData exposing (WebData, RemoteData(..))
+import Constants exposing (maxStars)
 import Models exposing (..)
 import Models.Ratings exposing (..)
 import Models.Pokemon exposing (..)
@@ -22,29 +23,19 @@ addOneUserVoteByUser userRatings pokemon total =
     total + extractOnePokemonFromRatingString userRatings.ratings pokemon.number
 
 
-sumVotesByUser : List Pokemon -> UserRatings -> TeamGenLetterVotes -> TeamGenLetterVotes
-sumVotesByUser pokelist userRatings teamGenLetterVotes =
+sumVotesByUser : List Pokemon -> Int -> UserRatings -> TeamGenLetterVotes -> TeamGenLetterVotes
+sumVotesByUser pokeList expectedVotes userRatings teamGenLetterVotes =
     let
         totalVotes =
-            List.foldl (addOneUserVoteByUser userRatings) 0 pokelist
-
-        expectedVotes =
-            if List.length pokelist == 0 then
-                0
-            else if List.length pokelist == 1 then
-                3
-            else if List.length pokelist == 2 then
-                5
-            else
-                6
+            List.foldl (addOneUserVoteByUser userRatings) 0 pokeList
 
         completionLevel =
-            if totalVotes == expectedVotes then
+            if totalVotes >= expectedVotes then
                 Complete
-            else if totalVotes == 0 then
-                Absent
-            else
+            else if totalVotes > 0 then
                 Incomplete
+            else
+                Absent
     in
         if userRatings.active || totalVotes > 0 then
             { userId = userRatings.id
@@ -60,20 +51,28 @@ sumVotesByUser pokelist userRatings teamGenLetterVotes =
 calculatePeopleVotes : ApplicationState -> TeamGenLetterVotes
 calculatePeopleVotes model =
     let
-        pokelist =
+        pokeList =
             filterPokedex
                 model.pokedex
                 model.subPage
 
-        teamRatings =
-            case model.ratings of
-                Success teamRatings ->
-                    teamRatings
+        maxNrPokemonToReceiveVotes : Int
+        maxNrPokemonToReceiveVotes =
+            List.length pokeList
+                |> clamp 0 maxStars
 
-                _ ->
-                    []
+        minStarsOnThisPage : Int
+        minStarsOnThisPage =
+            maxStars - maxNrPokemonToReceiveVotes + 1
+
+        expectedVotes : Int
+        expectedVotes =
+            List.range minStarsOnThisPage maxStars
+                |> List.foldr (+) 0
     in
-        List.foldl (sumVotesByUser pokelist) [] teamRatings
+        model.ratings
+            |> RemoteData.withDefault []
+            |> List.foldl (sumVotesByUser pokeList expectedVotes) []
 
 
 
@@ -106,7 +105,7 @@ sumVotesByPokemon teamRatings pokemon rankings =
 calculatePokemonVotes : ApplicationState -> PokeRankings
 calculatePokemonVotes model =
     let
-        pokelist =
+        pokeList =
             filterPokedex
                 model.pokedex
                 model.subPage
@@ -119,4 +118,4 @@ calculatePokemonVotes model =
                 _ ->
                     []
     in
-        List.foldl (sumVotesByPokemon teamRatings) [] pokelist
+        List.foldl (sumVotesByPokemon teamRatings) [] pokeList
