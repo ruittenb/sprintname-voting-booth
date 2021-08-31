80a81e642adf95fcc25e1307a8c30d13555e0d09
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2021-08-31 10:38:03 +0200

    Added descriptions for variants in gen V; better artwork for 618

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -3,7 +3,7 @@
  * Serviceworker for Pokémon Sprint Name Voting Booth
  */
 
-const version = 'v17.3.0';
+const version = 'v17.3.1';
 const cacheName = 'sprintname-voting-booth-' + version;
 const imageDir = '/pokeart/'
 const placeHolder = 'silhouette.png';
