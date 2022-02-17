8e11ff4e5f51d461d2548173a73be80e4145c17b
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2022-02-17 15:17:42 +0100

    Added new 'pages' incorporating Gen iii

--- data/makepages.pl
+++ data/makepages.pl
@@ -6,16 +6,17 @@ use POSIX qw(mktime strftime);
 
 use constant TWO_WEEKS => 60 * 60 * 24 * 14; # Get ready for a surprise!
 
-my ($sec, $min, $hour, $mday, $mon, $year) = (0, 0, 0, 25, 6, 123); # 2023-07-04T00:00:00.000+02:00
+my ($sec, $min, $hour, $mday, $mon, $year) = (0, 0, 0, 25, 5, 124); # 2024-06-25T00:00:00.000+02:00
 my $date = mktime($sec, $min, $hour, $mday, $mon, $year);
 
-my $id = 199;
-my $generation = "ii";
+my $id = 224;
+my $generation = "iii";
 
 for ("A" .. "Z") {
-    next if /U/;
+    next if /[IQUVZ]/;
 
-	my $datestr = strftime("%Y-%m-%dT00:00:00.000+02:00", localtime $date);
+    $date += TWO_WEEKS;
+	my $datestr = strftime("%Y-%m-%dT00:00:00.000%z", localtime $date);
     print <<"    _end_";
       },
       {
@@ -27,7 +28,6 @@ for ("A" .. "Z") {
     _end_
 
     $id++;
-    $date += TWO_WEEKS;
 }
 
 
