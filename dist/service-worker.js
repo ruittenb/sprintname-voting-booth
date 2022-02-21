eeb9a98440f1f5c2fa4c4e70ac717fe4e0851fa0
René Uittenbogaard <rene.uittenbogaard@proforto.nl>
2022-02-21 19:12:27 +0100

    Updated files with new tag

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -3,7 +3,7 @@
  * Serviceworker for Pokémon Sprint Name Voting Booth
  */
 
-const version = 'v19.1.0';
+const version = 'v19.2.0';
 const cacheName = 'sprintname-voting-booth-' + version;
 const imageDir = '/pokeart/'
 const placeHolder = 'silhouette.png';
